f5bcacc8e70abb501acc6bbc365f44228647e2b3
Namish <namishdev.gupta@gmail.com>
2026-01-01 13:11:01 +0000

    Create Arrays Section

--- Basics/Fibonacci Number/fibonaccinumber.cpp
+++ Basics/Fibonacci Number/fibonaccinumber.cpp
@@ -5,6 +5,18 @@ public:
         return fib(n - 1) + fib(n - 2);
     }
 
+    int fibBetter(int n) {
+        if (n == 0) return 0;
+        if (n == 1) return 1;
+        vector<int> dp(n + 1);
+        dp[0] = 0;
+        dp[1] = 1;
+        for (int i = 2; i <= n; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+        return dp[n];
+    }
+
     int fibOptimal(int n) {
         if(n==0 || n==1) return n;
         int a = 0, b = 1, sum = 0;
